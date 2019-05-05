bda96ede88557fd51248656b32a7346088f886b0
csmithy <csmith95@stanford.edu>
2019-05-05 12:58:32 -0700

    using different heuristic for ID matching. not bad, but still problems

--- process_data.py
+++ process_data.py
@@ -65,66 +65,40 @@ def fetchAndClean(dataPath):
 	df = pandas.read_csv(dataPath)
 	return df.rename(columnNameFixer, axis='columns')
 
-def saveSelectFeatures(df, features, path):
-	df[features].to_csv(path)
-
 def addIDs(df2017, lookup):
 	# imperfect matching, but the approach is to consider two players equal if
-	# they have the same last name, nationality. if any duplicates found, 
-	# throw these out
+	# they have the same first initial, last name, nationality.
 	for index, row in df2017.iterrows():
 		try:
+			firstInitial = row.Name.split()[0][0]
 			lastName = row.Name.split()[-1]
 			nationality = row.Nationality
-			club = row.Club.split()[0]
 		except:
 			continue
 
-		key = frozenset([lastName, nationality, club])
+		key = frozenset([firstInitial, lastName, nationality])
 		if key in lookup:
 			df2017.at[index, 'ID'] = lookup[key]
 
 	return df2017
 
-def buildLookup(df2018, df2019):
-	lookup1 = defaultdict(int)
-	for _, row in df2018.iterrows():
+# build a map from player key (first initial, last name, nationality)
+# to set of IDs (multiple IDs problem handles outside this function)
+def buildLookup(df, lookup=defaultdict(set)):
+	lookup = defaultdict(set)
+	for _, row in df.iterrows():
 		try:
+			firstInitial = row.Name.split()[0][0]
 			lastName = row.Name.split()[-1]
 			nationality = row.Nationality
-			club = row.Club.split()[0]
 		except:
+			print('excpetion')
 			continue
 
-		key = frozenset([lastName, nationality, club])
-		if key in lookup1:
-			# not unique :( let's just pretend this player doesn't exist
-			lookup1[key] = -1
-		else:
-			lookup1[key] = row.ID
+		key = frozenset([firstInitial, lastName, nationality])
+		lookup[key].add(row.ID)
 
-	lookup2 = defaultdict(int)
-	for _, row in df2019.iterrows():
-		try:
-			lastName = row.Name.split()[-1]
-			nationality = row.Nationality
-			club = row.Club.split()[0]
-		except:
-			continue
-
-		key = frozenset([lastName, nationality, club])
-		if key in lookup2:
-			# not unique :( let's just pretend this player doesn't exist
-			lookup2[key] = -1
-		else:
-			lookup2[key] = row.ID
-
-	allKeys = set(lookup1.keys()) | set(lookup2.keys())
-	uniqueCommonIDs = set(lookup1.values()) & set(lookup2.values())
-	combined = { k: max(lookup1[k], lookup2[k]) for k in allKeys if lookup1[k] in uniqueCommonIDs or lookup2[k] in uniqueCommonIDs}
-	print(len(combined.keys()))
-	return combined
-
+	return lookup
 
 def prune(df2017, df2018, df2019):
 	commonIDs = set(df2017['ID']) & set(df2018['ID']) & set(df2019['ID'])
@@ -143,23 +117,30 @@ print("2018 shape: ", df2018.shape)
 print("2019 shape: ", df2019.shape)
 
 # necessary because 2017 dataset doesn't include IDs
-df2017.insert(0, 'ID', -1) # add ID column with all set to -1
-lookup = buildLookup(df2017, df2018)
-df2017 = addIDs(df2017, lookup)
+lookup = buildLookup(df2018)
+lookup = buildLookup(df2019, lookup)
 
-success = df2017.loc[df2017['ID'] != -1]
-print('Num IDs matched in 2017 dataset {}'.format(success.shape))
+# disambiguation -- throw out any players matched with 
+# multiple IDs (this could happen because our heuristic
+# for matching players is initial, last name, nationality)
+cleaned = {}
+for key, idSet in lookup.items():
+	if len(idSet) > 1: continue
+	cleaned[key] = idSet.pop()
+
+df2017.insert(0, 'ID', -1) # add ID column with all set to -1
+df2017 = addIDs(df2017, cleaned)
+
+withIDs2017 = df2017.loc[df2017['ID'] != -1]
+print('Num IDs added to 2017 dataset ', withIDs2017.shape[0])
 
 # remove any players that don't exist in all 3 datasets
-df2017, df2018, df2019 = prune(df2017, df2018, df2019)
+print("Removing players/features that aren't present in all 3 datasets...")
+df2017, df2018, df2019 = prune(withIDs2017, df2018, df2019)
 
 features2017 = set(df2017.columns)
 features2018 = set(df2018.columns)
 features2019 = set(df2019.columns)
-
-print("pruned 2017 shape: ", df2017.shape)
-print("pruned 2018 shape: ", df2018.shape)
-print("pruned 2019 shape: ", df2019.shape)
 
 commonFeatures = features2017 & features2018 & features2019
 for unencodedFeature in unencoded:
@@ -169,6 +150,14 @@ for unencodedFeature in unencoded:
 		print('KeyError: ', e)
 
 commonFeatures = list(commonFeatures)
-# saveSelectFeatures(df2017, commonFeatures, './data/2017CommonFeatures.csv')
-# saveSelectFeatures(df2018, commonFeatures, './data/2018CommonFeatures.csv')
-# saveSelectFeatures(df2019, commonFeatures, './data/2019CommonFeatures.csv')
+df2017 = df2017[commonFeatures]
+df2018 = df2018[commonFeatures]
+df2019 = df2019[commonFeatures]
+print("pruned 2017 shape: ", df2017.shape)
+print("pruned 2018 shape: ", df2018.shape)
+print("pruned 2019 shape: ", df2019.shape)
+print('Features: ', commonFeatures)
+
+# df2017.to_csv('./data/2017CommonFeatures.csv')
+# df2018.to_csv('./data/2018CommonFeatures.csv')
+# df2019.to_csv('./data/2019CommonFeatures.csv')
